8add188d6b256caaccfe80055d5e49a6371a7b25
HienNguyen120601 <nguyen.nguyenhienth05@gmail.com>
2023-07-24 23:11:18 +0700

    Change status invoice

--- src/libs/core/constants/url.constant.ts
+++ src/libs/core/constants/url.constant.ts
@@ -1,10 +1,10 @@
-// export const API_ROLE = 'http://localhost:3001/api/v1/role';
-// export const API_USER = 'http://localhost:3001/api/v1/user';
-// export const API_TOUR = 'http://localhost:3001/api/v1/tour';
-// export const API_LOCATION = 'http://localhost:3001/api/v1/location';
-// export const API_COMMENT = 'http://localhost:3001/api/v1/tour-comment';
-// export const API_INVOICE = 'http://localhost:3001/api/v1/invoice';
-// export const API_CONTACT = 'http://localhost:3001/api/v1/contact';
+export const API_ROLE = 'http://localhost:3001/api/v1/role';
+export const API_USER = 'http://localhost:3001/api/v1/user';
+export const API_TOUR = 'http://localhost:3001/api/v1/tour';
+export const API_LOCATION = 'http://localhost:3001/api/v1/location';
+export const API_COMMENT = 'http://localhost:3001/api/v1/tour-comment';
+export const API_INVOICE = 'http://localhost:3001/api/v1/invoice';
+export const API_CONTACT = 'http://localhost:3001/api/v1/contact';
 
 export const API_UPLOAD =
   'https://ioypxsbwog.execute-api.ap-southeast-1.amazonaws.com/dev/trippy-storage/';
